354bd8bdbe07cdeefa0df3d8d66a1ca213dba01d
Илья Гадалов <gadaloffilya@yandex.ru>
2020-09-22 18:09:04 +0700

    Создание View-Model для MainWindow и создание обработчика с заголовком окна

---
+++ ControlSpendings/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ControlSpendings.ViewsModels.Base;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ControlSpendings.ViewsModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна 
+        
+        /// <summary> Заголовок окна </summary>
+        private string _Title;
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
