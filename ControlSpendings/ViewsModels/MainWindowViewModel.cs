4b019bfc30a305855eafae70d5858273c3c7b325
Илья Гадалов <gadaloffilya@yandex.ru>
2020-09-23 14:46:03 +0700

    Создание первой комманды и привязка к элементу в интерфейсе

--- ControlSpendings/ViewsModels/MainWindowViewModel.cs
+++ ControlSpendings/ViewsModels/MainWindowViewModel.cs
@@ -1,13 +1,16 @@
-﻿using ControlSpendings.ViewsModels.Base;
+﻿using ControlSpendings.Infrastructure.Commands;
+using ControlSpendings.ViewsModels.Base;
 using System;
 using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ControlSpendings.ViewsModels
 {
     internal class MainWindowViewModel : ViewModel
     {
         #region Заголовок окна 
-        
+
         /// <summary> Заголовок окна </summary>
         private string _Title = "Главное окно - ControlS";
 
@@ -18,5 +21,28 @@ namespace ControlSpendings.ViewsModels
         }
 
         #endregion
+
+        #region Commands
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted(object p) => Application.Current.Shutdown();
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Commands
+
+            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+
+
+        }
     }
 }
